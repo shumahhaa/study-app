4d64ce62ab9e839d5e5ba7437e72fc076c8c1e9c
shumahhaa <satoshu0213@gmail.com>
2025-03-13 22:00:49 +0900

    タイトルと学習内容の作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [studyTopic, setStudyTopic] = useState("");
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>学習管理アプリ</h1>
+      <input type="text" value={studyTopic} onChange={(e)=> setStudyTopic(e.target.value)} placeholder="何を学習する？"　/>
     </div>
-  );
+  )
 }
 
 export default App;
