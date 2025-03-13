47269d601bbcf8177af8f07ee16c753bdd9f2e07
shumahhaa <satoshu0213@gmail.com>
2025-03-13 22:22:30 +0900

    学習開始ボタンの追加

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 function App() {
   const [studyTopic, setStudyTopic] = useState("");
+  const [isStudying, setIsStudying] = useState(false);
+
   return (
     <div>
       <h1>学習管理アプリ</h1>
-      <input type="text" value={studyTopic} onChange={(e)=> setStudyTopic(e.target.value)} placeholder="何を学習する？"　/>
+      <input 
+        type="text"
+        value={studyTopic}
+        onChange={(e)=> setStudyTopic(e.target.value)}
+        placeholder="何を学習する？"
+      />
+
+      <button
+        onClick={() => setIsStudying(true)}
+        disabled={isStudying || studyTopic.trim() === ""}
+      >
+        学習開始
+      </button>
+
+      {isStudying && <p>「{studyTopic}の学習を開始しました！」</p>}
     </div>
   )
 }
