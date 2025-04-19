ebcedbdc45f7525a8f5a02d741ea110f96ad8931
shumahhaa <satoshu0213@gmail.com>
2025-04-19 19:28:06 +0900

    🎨 デバック用のコード追加コード追加

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// デバッグ用：環境変数の値をコンソールに表示
+console.log('環境変数一覧:', {
+  REACT_APP_MAX_USAGE_COUNT: process.env.REACT_APP_MAX_USAGE_COUNT,
+  REACT_APP_SELECTED_MODEL: process.env.REACT_APP_SELECTED_MODEL,
+  REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
